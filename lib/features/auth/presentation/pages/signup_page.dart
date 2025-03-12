57c073e6bfc15c714895f98f8e5a5dc7dbe87895
Anh Quoc <badguykendf@gmail.com>
2025-03-13 02:06:33 +0700

    feat: Form for signup page

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -11,43 +11,63 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 30),
-            AuthField(hintText: 'Name'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Email'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Password'),
-            SizedBox(height: 20),
-            AuthGradientButton(),
-            SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: 'Have an account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                    text: 'Sign In',
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: AppPallete.gradient2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              AuthField(hintText: 'Name', controller: nameController),
+              SizedBox(height: 15),
+              AuthField(hintText: 'Email', controller: emailController),
+              SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              SizedBox(height: 20),
+              AuthGradientButton(),
+              SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

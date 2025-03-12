20d2cb676e3d280e7e7ac58af745bcd77c8d8d16
Anh Quoc <badguykendf@gmail.com>
2025-03-13 02:46:55 +0700

    feat: Setup supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://xliwozkxlwnrktbfqwuf.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsaXdvemt4bHducmt0YmZxd3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3OTYwNTgsImV4cCI6MjA1NzM3MjA1OH0.h7eQR-f_izjhsgThijQa2e1p8QQAlngWsx6f0t9qFRk';
+}
